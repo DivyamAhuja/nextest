24f7ff07d8ffc24fd3fd274c6189aecd9946c6f6
Divyam Ahuja <ahujadivyam@gmail.com>
2022-06-21 01:06:49 +0530

    hmm

--- cms/config.js
+++ cms/config.js
@@ -1,40 +1,36 @@
 export default {
     cms_manual_init: true,
     backend: {
-      name: 'github',
-      repo: 'DivyamAhuja/nextest',
-      branch: 'main',
+        name: 'github',
+        repo: 'DivyamAhuja/nextest',
+        branch: 'main',
     },
     media_folder: 'public/img',
     public_folder: 'img',
     collections: [
-      {
-        name: 'pages',
-        label: 'Pages',
-        files: [
-          {
-            label: 'Home',
-            name: 'home',
-            file: 'content/pages/home.md',
+        {
+            name: 'pages',
+            label: 'Pages',
+            folder: 'content/pages',
+            create: true,
             fields: [
-              {
-                label: 'Hero Title',
-                name: 'hero_title',
-                widget: 'string',
-              },
-              {
-                label: 'Hero Description',
-                name: 'hero_description',
-                widget: 'markdown',
-              },
-              {
-                label: 'Hero Image',
-                name: 'hero_image',
-                widget: 'image'
-              },
+                {
+                    label: 'Hero Title',
+                    name: 'hero_title',
+                    widget: 'string',
+                },
+                {
+                    label: 'Hero Description',
+                    name: 'hero_description',
+                    widget: 'markdown',
+                },
+                {
+                    label: 'Hero Image',
+                    name: 'hero_image',
+                    widget: 'image'
+                },
+
             ],
-          },
-        ],
-      },
+        },
     ],
-  };
+};
